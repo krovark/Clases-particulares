752eefe489e82e3927c57b27493d71748003ed66
krovark <santglez51@gmail.com>
2023-09-20 18:14:05 -0300

    first commit

---
+++ src/Perfil/perfil-site.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import NavigationBar from '../HomeMenu/NavBar';
+import ProfileInfo from './profile-content'
+
+const userProfile = () => {
+    return ( 
+
+        <div className="big-container-profile">
+        
+            <ProfileInfo></ProfileInfo>
+
+        </div>
+
+
+);
+}
+ 
+export default userProfile;
